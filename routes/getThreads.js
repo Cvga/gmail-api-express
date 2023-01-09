89a6a6f268806a55d68226e0bc1390c26fc786bb
Daulyn Rosado <daulyn@gmail.com>
2023-01-09 10:35:29 -0400

    Add headers

--- routes/getThreads.js
+++ routes/getThreads.js
@@ -8,6 +8,15 @@ const keys = require("../credentials.json");
 const router = express.Router();
 
 router.get("/authenticate", async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
+
   try {
     const oAuth2Client = new OAuth2Client(
       keys.web.client_id,
