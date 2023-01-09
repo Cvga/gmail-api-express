2c775217cfe22deb85743ad4b480eeef4c5b4b94
Daulyn Rosado <daulyn@gmail.com>
2023-01-09 09:15:19 -0400

    implement cors

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const express = require ('express');
-const { json } = require('body-parser');
-const { urlencoded } = require('express');
-const getThreads = require('./routes/getThreads');
-const getThreadId = require('./routes/getThreadId');
-const morgan = require('morgan');
+const express = require("express");
+const { json } = require("body-parser");
+const { urlencoded } = require("express");
+const getThreads = require("./routes/getThreads");
+const getThreadId = require("./routes/getThreadId");
+const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(json());
-app.use(urlencoded({extended:true}));
-app.use(morgan('tiny'))
+app.use(urlencoded({ extended: true }));
+app.use(morgan("tiny"));
 
 app.use(getThreads);
 app.use(getThreadId);
-
 
 module.exports = app;
